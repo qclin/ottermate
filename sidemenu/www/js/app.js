0f203cc964d0137b8710d52b94dfae76048baa0d
Evan Griffiths <griffithse@gmail.com>
2015-05-27 02:08:02 -0400

    updated sidemenu project... fixed some bugs and added more pages

--- sidemenu/www/js/app.js
+++ sidemenu/www/js/app.js
@@ -1,45 +1,63 @@
 angular.module('ionicApp', ['ionic'])
+  .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function() {
+      $ionicSideMenuDelegate.toggleLeft()   
+    }
+  })
 
-    .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-        $scope.toggleLeft = function() {
-             $ionicSideMenuDelegate.toggleLeft()   
+  .controller("LoginController", function($scope,$state) {
+    $scope.login = function() {
+      $state.go('menu.profile');
+    };
+  })
+
+  .controller("PostRoomController", function($scope) {
+    $scope.posts = ["here","is","some","dummy","data"];
+  })
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    $stateProvider
+      // states that don't have a sidemenu
+      .state("login", {
+        url: "/login",
+        templateUrl: "templates/login.html"
+      })
+      .state("signup", {
+        url: "/signup",
+        templateUrl: "templates/signup.html"
+      })
+
+      // states that include a sidemenu
+      .state("menu", {
+        url: "/menu",
+        // abstract causes this state to only be accessible through it's child states
+        abstract: true,
+        templateUrl: "templates/menu.html"
+      })
+      .state("menu.profile", {
+        url: "/profile",
+        views: {
+          "menuContent": {
+            templateUrl: "templates/profile.html"
+          }
         }
-    })
-
-    .config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/menu/home");
-
-        $stateProvider
-            .state('menu', {
-                url: "/menu",
-                abstract: true,
-                templateUrl: "templates/menu.html"
-            })
-            .state('menu.home', {
-                url: "/home",
-                views: {
-                    'menuContent' :{
-                        templateUrl: "templates/home.html"
-                    }
-                }
-            });
-    })
-
-    .config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("menu/profile");
-        
-        $stateProvider
-            .state('menu', {
-                url: "/menu",
-                abstract: true,
-                templateUrl: "templates/menu.html"
-        })
-            .state('menu.profile', {
-                url: "/profile",
-                views: {
-                    'menuContent'  :{
-                        templateUrl: "templates/profile.html"
-                    }
-                }
-        });
-})
+      })
+      .state("menu.chatHistory", {
+        url: "/chatHistory",
+        views: {
+          "menuContent": {
+            templateUrl: "templates/chatHistory.html"
+          }
+        }
+      })
+      .state("menu.postRoom", {
+        url: "/postRoom",
+        views: {
+          "menuContent": {
+            templateUrl: "templates/postRoom.html"
+          }
+        }
+      });
+  })
