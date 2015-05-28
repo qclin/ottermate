a37138d9ce0099551c1801a801b1465b3f6a907c
Qiao Lin <qiaoclin@gmail.com>
2015-05-28 18:43:30 -0400

    token

--- matematch/app/controllers/auth_controller.rb
+++ matematch/app/controllers/auth_controller.rb
@@ -9,15 +9,13 @@ class AuthController < ApplicationController
     @user = User.find_by(username: username)
 
     if @user && @user.authenticate(password)
-      session[:user_id] = @user.id
-      redirect_to(user_path(@user))
-    else
-      flash[:error] = "Unknown User"
-      redirect_to(login_path)
-    end
+      # return encrypted token to angular app
+      render json: {token: makeToken(@user.id)}
 
-    # return encrypted token to angular app
-    render json: {token: makeToken(1)}
+    else
+      render json: {'authorized':'false'}, :status => 401
+
+    end
   end
 
   def test
--- matematch/app/controllers/current_users_controller.rb
+++ matematch/app/controllers/current_users_controller.rb
@@ -3,4 +3,15 @@ class CurrentUsersController < ApplicationController
   def show
     render json: User.find(currentUserId)
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      head :no_content
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
 end
